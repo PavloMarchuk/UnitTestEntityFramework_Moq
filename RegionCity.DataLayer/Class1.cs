56c2ed055c47491b99c6b92b1e6b313f15febc24
Oleksandr P <o.parfeniuk@gmail.com>
2017-09-28 11:19:41 +0300

    Add project files.

---
+++ RegionCity.DataLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionCity.DataLayer
+{
+    public class Class1
+    {
+    }
+}
